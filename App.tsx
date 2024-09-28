291e6ddc2201e4531a6b2009d61167c88d55208c
redemtiom <smar.pre.sky@gmail.com>
2024-09-28 17:40:37 -0600

    :heavy_plus_sign: :wrench: [feat]: add redux persit dependencies and configuration

--- App.tsx
+++ App.tsx
@@ -9,16 +9,19 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import AppNavigator from './src/navigators/app.navigator';
-import {store} from './src/store/store';
+import {store, persistor} from './src/store/store';
 
 const App = (): React.JSX.Element => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
