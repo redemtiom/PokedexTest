143fbc43df6ed7b330d934535e107e517b9431e1
redemtiom <smar.pre.sky@gmail.com>
2024-09-28 16:01:34 -0600

    :heavy_plus_sign: :wrench: [feat]: add redux toolkit dependencies and configuration files

--- App.tsx
+++ App.tsx
@@ -8,13 +8,18 @@
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+
 import AppNavigator from './src/navigators/app.navigator';
+import {store} from './src/store/store';
 
 const App = (): React.JSX.Element => {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
