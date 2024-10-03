ca68ed45f3e40ad0a5f20fa0e45b3962a2259dc0
redemtiom <smar.pre.sky@gmail.com>
2024-10-02 18:16:24 -0600

    :heavy_plus_sign: :sparkles: [feat]: add user feature

--- src/navigators/app.navigator.tsx
+++ src/navigators/app.navigator.tsx
@@ -3,13 +3,21 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import AppScreen from '../views/screens/AppScreen/app.screen';
+import UserScreen from '../views/screens/UserScreen/user.screen';
+import PokedexScreen from '../views/screens/PokedexScreen/pokedex.screen';
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator
+      initialRouteName="User"
+      screenOptions={{
+        headerShown: false,
+      }}>
       <Stack.Screen name="Home" component={AppScreen} />
+      <Stack.Screen name="User" component={UserScreen} />
+      <Stack.Screen name="Pokedex" component={PokedexScreen} />
     </Stack.Navigator>
   );
 };
---
+++ src/views/screens/PokedexScreen/pokedex.screen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const PokedexScreen = () => {
+  return (
+    <View>
+      <Text>Pokedex</Text>
+    </View>
+  );
+};
+
+export default PokedexScreen;
